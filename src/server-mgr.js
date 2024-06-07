cde7acb01ff9829024e0469c17b46a7fdb4f9450
黄志伟 <huangzhiwei@ydjdev.com>
2024-06-07 15:52:49 +0800

    dir

--- src/server-mgr.js
+++ src/server-mgr.js
@@ -1,10 +1,10 @@
 /**
  * @author: HZWei
  * @date: 2024/5/22
- * @desc:
+ * @desc: 公司定制需求
  */
 
-const {initConfig, configFilePath, configOutputFilePath, configReleaseFilePath,manifestPath} = require("./file-mgr")
+const {initConfig, configFilePath, configOutputFilePath, configReleaseFilePath, manifestPath} = require("./file-mgr")
 if (initConfig()) return;
 
 const configRelease = require(configReleaseFilePath)
@@ -32,9 +32,9 @@ const api = {
         devBaseUrl: "https://cim-test-api.yidejia.com/",
         betaBaseUrl: "https://cim-uat-api.yidejia.com/",
         releaseBaseUrl: "https://cim-api.yidejia.com/",
-        devSign:"cpa4mqkuuf81l1qfgvqg",
-        betaSign:"cpapae1jvotmrd3gcq30",
-        releaseSign:"cpapaln18np8tarnts7g",
+        devSign: "cpa4mqkuuf81l1qfgvqg",
+        betaSign: "cpapae1jvotmrd3gcq30",
+        releaseSign: "cpapaln18np8tarnts7g",
         commonConfig: "common/config",
         login: "user/login/code",
         baseUrlArray: [],
@@ -57,14 +57,14 @@ const axios = require('axios');
 axios.defaults.baseURL = api.baseUrl
 axios.interceptors.request.use((config) => {
     // console.log("enableServerLog: ",enableServerLog)
-    if (enableServerLog){
+    if (enableServerLog) {
         console.log("")
         console.log("============ request ==============")
-        console.log(`${config.baseURL}${config.url}`,config.method)
-        console.log('data: ',config.data)
+        console.log(`${config.baseURL}${config.url}`, config.method)
+        console.log('data: ', config.data)
         console.log("")
     }
-    if (config.baseURL ===  api.baseUrl) {
+    if (config.baseURL === api.baseUrl) {
         if (config.url !== api.login) {
             config.headers["Authorization"] = `bearer ${api.token}`
         }
@@ -79,17 +79,17 @@ axios.interceptors.request.use((config) => {
 })
 
 
-axios.interceptors.response.use( async (response) => {
+axios.interceptors.response.use(async (response) => {
     const baseURL = response.config.baseURL
-    if (enableServerLog){
+    if (enableServerLog) {
         console.log("============ response ==============")
-        console.log(`${baseURL}${response.config.url}`,response.config.method, response.status)
+        console.log(`${baseURL}${response.config.url}`, response.config.method, response.status)
         console.log('data: ', response.data)
     }
     switch (response.status) {
         case 200 :
             const code = response.data.code
-            if (baseURL === api.baseUrl){
+            if (baseURL === api.baseUrl) {
                 if (code === 200 && response.config.url === api.login) {
                     api.token = response.data.data
                     let data = fs.readFileSync(configReleaseFilePath, "utf-8")
@@ -99,7 +99,7 @@ axios.interceptors.response.use( async (response) => {
                     fs.writeFileSync(configReleaseFilePath, data, "utf-8");
                     return response.data?.data ?? {}
                 }
-                if (code === 401){
+                if (code === 401) {
                     const max = 3
                     for (let i = 0; i < max; i++) {
                         api.configs.token = await loginPlatform()
@@ -108,9 +108,9 @@ axios.interceptors.response.use( async (response) => {
                 }
             }
 
-            if (api.configs.baseUrlArray.includes(baseURL)){
-                if (code === 0){
-                    if (response.config.url === api.configs.login){
+            if (api.configs.baseUrlArray.includes(baseURL)) {
+                if (code === 0) {
+                    if (response.config.url === api.configs.login) {
                         api.configs.token = response.data.data.token
                         let data = fs.readFileSync(configReleaseFilePath, "utf-8")
                         const json = JSON.parse(data)
@@ -125,23 +125,28 @@ axios.interceptors.response.use( async (response) => {
     }
     return response.data?.data ?? null
 }, async (err) => {
-    const config = err.response.config
-    const data = err.response.data
-    if (enableServerLog){
-        console.error("=========== response err ===========")
-        console.error(`${config.baseURL}${config.url}`, config.method)
-        console.error(data)
-    }
+    try {
+        const config = err.response?.config
+        const data = err.response?.data
+        if (enableServerLog) {
+            console.error("=========== response err ===========")
+            console.error(`${config.baseURL}${config.url}`, config.method)
+            console.error(data)
+        }
 
-    const max = 3
-    if (api.configs.baseUrlArray.includes(config.baseURL)) {
-        if (data.code === 1) {
-            for (let i = 0; i < max; i++) {
-                api.configs.token = await loginMall()
-                if (api.configs.token) break
+        const max = 3
+        if (api.configs.baseUrlArray.includes(config.baseURL)) {
+            if (data.code === 1) {
+                for (let i = 0; i < max; i++) {
+                    api.configs.token = await loginMall()
+                    if (api.configs.token) break
+                }
             }
         }
+    } catch (e) {
+
     }
+
     // return data ?? {code: -1, data: null, message: err.response.statusText}
     return null
 })
@@ -167,118 +172,147 @@ async function loginMall() {
     }
 
     let params = {
-        phone: api.configs.username,
-        code: api.configs.password,
-        area_code: "+86",
-        sign: sign,
-        app_version: "1.0.0"
+        phone: api.configs.username, code: api.configs.password, area_code: "+86", sign: sign, app_version: "1.0.0"
     }
     const data = await axios.post(api.configs.login, params, {baseURL: baseUrl})
     return data.token
 }
 
-
-
-async function updateConfig(params = {
-    versionCode: 1,
-    versionName: '1.0.0',
-    url: '',
+async function getServerConfig(params = {
     dev: false, beta: false, release: false
 }) {
-
-    // const baseUrl = api.configs.devBaseUrl
-    // axios.defaults.baseURL = baseUrl
-    api.configs.dev = params.dev
-    api.configs.beta =  params.beta
-    api.configs.release =  params.release
-    let baseUrl = api.configs.devBaseUrl
-    if (params.dev) baseUrl = api.configs.devBaseUrl
-    if (params.beta) baseUrl = api.configs.betaBaseUrl
-    if (params.release) baseUrl = api.configs.releaseBaseUrl
-
-
-    function envInfo(){
-        console.log(`key: ${api.configs.key}`)
-        if (params.dev) console.error(`dev: ${params.dev}`)
-        if (params.beta) console.error(`beta: ${params.beta}`)
-        if (params.release) console.error(`release: ${params.release}`)
-        console.error(`baseUrl: ${baseUrl}`)
-        console.error(``)
-    }
-
-
-    // 获取config配置
+    let baseUrl = initEnvironment(params)
     const key = api.configs.key
     const res = await axios.get(`${api.configs.commonConfig}?key=${key}`, {baseURL: baseUrl})
-    if (!res || !res.data){
+    if (!res || !res.data) {
         console.log(`========== 检查后台是否有配置: ${key}  ==========`)
-        envInfo()
+        envInfo(baseUrl, params)
         return
     }
     let data = {}
     try {
-         data = JSON.parse(res.data) ?? {}
-    }catch (e) {
+        data = JSON.parse(res.data) ?? {}
+    } catch (e) {
 
     }
+    return data
+}
+
+function initEnvironment(params = {
+    dev: false, beta: false, release: false
+}) {
+    api.configs.dev = params.dev
+    api.configs.beta = params.beta
+    api.configs.release = params.release
+    let baseUrl = api.configs.devBaseUrl
+    if (params.dev) baseUrl = api.configs.devBaseUrl
+    if (params.beta) baseUrl = api.configs.betaBaseUrl
+    if (params.release) baseUrl = api.configs.releaseBaseUrl
+    return baseUrl
+}
+
+function envInfo(baseUrl, params = {
+    dev: false, beta: false, release: false
+}) {
+    console.log(`key: ${api.configs.key}`)
+    if (params.dev) console.error(`dev: ${params.dev}`)
+    if (params.beta) console.error(`beta: ${params.beta}`)
+    if (params.release) console.error(`release: ${params.release}`)
+    console.error(`baseUrl: ${baseUrl}`)
+    console.error(``)
+}
+
+
+async function updateConfig(params = {
+    versionCode: 1, versionName: '1.0.0', url: '', dev: false, beta: false, release: false
+}) {
+
+    // const baseUrl = api.configs.devBaseUrl
+    // axios.defaults.baseURL = baseUrl
+    // api.configs.dev = params.dev
+    // api.configs.beta = params.beta
+    // api.configs.release = params.release
+    // let baseUrl = api.configs.devBaseUrl
+    // if (params.dev) baseUrl = api.configs.devBaseUrl
+    // if (params.beta) baseUrl = api.configs.betaBaseUrl
+    // if (params.release) baseUrl = api.configs.releaseBaseUrl
+
+    let baseUrl = initEnvironment(params)
+
+    // function envInfo(){
+    //     console.log(`key: ${api.configs.key}`)
+    //     if (params.dev) console.error(`dev: ${params.dev}`)
+    //     if (params.beta) console.error(`beta: ${params.beta}`)
+    //     if (params.release) console.error(`release: ${params.release}`)
+    //     console.error(`baseUrl: ${baseUrl}`)
+    //     console.error(``)
+    // }
+
+
+    // 获取config配置
+    // const key = api.configs.key
+    // const res = await axios.get(`${api.configs.commonConfig}?key=${key}`, {baseURL: baseUrl})
+    // if (!res || !res.data) {
+    //     console.log(`========== 检查后台是否有配置: ${key}  ==========`)
+    //     envInfo()
+    //     return
+    // }
+    // let data = {}
+    // try {
+    //     data = JSON.parse(res.data) ?? {}
+    // } catch (e) {
+    //
+    // }
+    let data = await getServerConfig(params)
     // 查找对应的内容  插入新配置或替换
     let version = params.versionName
     let code = params.versionCode
     let url = params.url
-    if (!data || !data.uniModuleArray){
+    if (!data || !data.uniModuleArray) {
         data = {
-            uniModuleArray:[]
+            uniModuleArray: []
         }
     }
     let item = data?.uniModuleArray?.find((value) => value.appid === uniAppId)
-    if (item){
+    if (item) {
         // 修改
         item.desc = description
         item.updateTime = new Date().toLocaleString()
-        if (api.configs.android){
+        item.updateUser = process.env.USER
+        if (api.configs.android) {
             item.android = {
-                version: version,
-                code: Number.parseInt(code),
-                updateDesc: api.configs.updateDesc,
-                url: url
+                version: version, code: Number.parseInt(code), updateDesc: api.configs.updateDesc, url: url
             }
         }
-        if (api.configs.ios){
+        if (api.configs.ios) {
             item.ios = {
-                version: version,
-                code: Number.parseInt(code),
-                updateDesc: api.configs.updateDesc,
-                url: url
+                version: version, code: Number.parseInt(code), updateDesc: api.configs.updateDesc, url: url
             }
         }
-    }else {
+    } else {
         // 新增
         const newItem = {
-            moduleName: name,
-            appid: uniAppId,
-            desc: description
+            moduleName: name, appid: uniAppId, desc: description
         }
         newItem.android = {
-            version: version,
-            code: Number.parseInt(code),
-            updateDesc: api.configs.updateDesc,
-            url: url
+            version: version, code: Number.parseInt(code), updateDesc: api.configs.updateDesc, url: url
         }
         newItem.ios = newItem.android
         newItem.updateTime = new Date().toLocaleString()
+        newItem.updateUser = process.env.USER
         data?.uniModuleArray?.push(newItem)
     }
     // 更新
     const updateParam = {
-        option_key: key, data: JSON.stringify(data,null,2)
+        option_key: key, data: JSON.stringify(data, null, 2)
     }
     const max = 3
     for (let i = 0; i < max; i++) {
         let updateRes = await axios.post(api.configs.commonConfig, updateParam, {baseURL: baseUrl})
-        if (updateRes){
+        if (updateRes) {
             console.log(`==========  后台配置同步完成 结果如下  ==========`)
-            envInfo()
-            console.log(JSON.stringify(data,null,2))
+            envInfo(baseUrl, params)
+            console.log(JSON.stringify(data, null, 2))
             break
         }
     }
@@ -292,7 +326,7 @@ async function loginPlatform() {
     const loginParam = {
         username, password
     }
-    return await axios.post(api.login, loginParam,{ baseURL: api.baseUrl})
+    return await axios.post(api.login, loginParam, {baseURL: api.baseUrl})
 }
 
 async function refreshCdnCache(url) {
@@ -310,8 +344,8 @@ async function refreshCdnCache(url) {
             break
         }
     }
-    let res = await axios.get(`${api.refresh_history}?operator_type=qiniu_refresh`,{baseURL: api.baseUrl})
-    if (config.enableRefreshHistory){
+    let res = await axios.get(`${api.refresh_history}?operator_type=qiniu_refresh`, {baseURL: api.baseUrl})
+    if (config.enableRefreshHistory) {
         console.log(`========== 刷新历史记录 ==========`)
         console.log(res)
         console.log('')
@@ -325,7 +359,7 @@ async function syncServer(params = {
 }) {
     if (!config.refreshUrl) return
     if (!api.username || !api.password || !api.configs.username || !api.configs.password || !api.baseUrl || !api.configs.key) {
-       console.log("========== 检查配置是否正常 ==========")
+        console.log("========== 检查配置是否正常 ==========")
         return
     }
     const manifest = require(manifestPath)
